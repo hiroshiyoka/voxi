752d972c3029ece4ea682f742ad24248246442d2
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-05 06:09:32 +0700

    feat(home): add conditional with state hook to display the Features component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import Features from "../components/Features";
 
 const HomeScreen = () => {
+  const [messages, setMessages] = useState([]);
+
   return (
     <View className="flex-1 bg-white">
       <SafeAreaView className="flex flex-1 mx-5">
@@ -16,6 +19,9 @@ const HomeScreen = () => {
             style={{ height: hp(15), width: wp(15) }}
           />
         </View>
+
+        {/* Features || Messages */}
+        {messages.length > 0 ? <View></View> : <Features />}
       </SafeAreaView>
     </View>
   );

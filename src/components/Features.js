905bec635cf2b82c9851bfd29a83ad6b86a0c523
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-05 05:46:23 +0700

    feat(features): add the responsive screen function into Features component

---
+++ src/components/Features.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
+const Features = () => {
+  return (
+    <View>
+      <Text>Features</Text>
+    </View>
+  );
+};
+
+export default Features;

6b3d7a1f86cccb6a632a20173df4499137c1d975
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-05 05:39:48 +0700

    feat(home): add react-native-responsive-screen width and height function

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const HomeScreen = () => {
   return (

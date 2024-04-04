068cc953ab82ffecb459f95554ea56d75f11b2e1
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-05 05:32:38 +0700

    feat(home): add responsive fontSize for title and subtitle in WelcomeScreen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -6,13 +6,23 @@ import {
   TouchableOpacity,
 } from "react-native";
 import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const WelcomeScreen = () => {
   return (
     <SafeAreaView className="flex flex-1 justify-around bg-white">
       <View className="space-y-2">
-        <Text className="text-center font-bold text-gray-700">Voxi</Text>
-        <Text className="text-center tracking-wider text-gray-600 font-semibold">
+        <Text
+          style={{ fontSize: wp(10) }}
+          className="text-center font-bold text-gray-700">
+          Voxi
+        </Text>
+        <Text
+          style={{ fontSize: wp(4) }}
+          className="text-center tracking-wider text-gray-600 font-semibold">
           The Future is here, powered by AI.
         </Text>
       </View>

ea1dadeed3646dcf8934c2227979a86a3b95b857
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-06 00:55:54 +0700

    feat(home): display the dummy messages to messages component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -35,7 +35,48 @@ const HomeScreen = () => {
               <ScrollView
                 bounces={false}
                 className="space-y-4"
-                showsVerticalScrollIndicator={false}></ScrollView>
+                showsVerticalScrollIndicator={false}>
+                {messages.map((message, index) => {
+                  if (message.role == "assistant") {
+                    if (message.content.includes(https)) {
+                      // AI Image
+                      return (
+                        <View key={index} className="flex-row justify-start">
+                          <View className="p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none">
+                            <Image
+                              source={{ uri: message.content }}
+                              className="rounded-2xl"
+                              resizeMode="contain"
+                              style={{ height: hp(60), width: wp(60) }}
+                            />
+                          </View>
+                        </View>
+                      );
+                    } else {
+                      // Text Response
+                      return (
+                        <View
+                          key={index}
+                          style={{ width: wp(70) }}
+                          className="bg-emerald-100 rounded-xl p-2 rounded-tl-none">
+                          <Text>{message.content}</Text>
+                        </View>
+                      );
+                    }
+                  } else {
+                    // User input
+                    return (
+                      <View key={index} className="flex-row justify-end">
+                        <View
+                          style={{ width: wp(70) }}
+                          className="bg-white rounded-xl p-2 rounded-tr-none">
+                          <Text>{message.content}</Text>
+                        </View>
+                      </View>
+                    );
+                  }
+                })}
+              </ScrollView>
             </View>
           </View>
         ) : (

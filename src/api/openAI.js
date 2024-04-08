3e8a822a21fde02c3118e99ba749e79361bc709c
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-08 07:37:11 +0700

    feat(api): add api call async function for the chatgpt prompt and messages

--- src/api/openAI.js
+++ src/api/openAI.js
@@ -11,3 +11,20 @@ const client = axios.create({
 
 const chatGptEndpoint = "https://api.openai.com/v1/chat/completions";
 const dalleEndpoint = "https://api.openai.com/v1/images/generations";
+
+export const apiCall = async (prompt, messages) => {
+  try {
+    const result = await client.post(chatGptEndpoint, {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Does this message want to generate an AI picture, image, art or anything similar? ${prompt} . Simply answer with a yes or no.`,
+        },
+      ],
+    });
+  } catch (error) {
+    console.log("Error: ", error);
+    return Promise.resolve({ success: false, message: error.message });
+  }
+};
